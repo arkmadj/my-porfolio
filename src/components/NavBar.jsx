47ddca52e115fd669e5edf37147c8c450d6875f6
arkmadj <ahmadjinadu@gmail.com>
2023-04-06 23:54:15 +0100

    added active link indicator

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import Logo from "./Logo";
+import { useRouter } from "next/router";
 
 const CustomLink = ({ href, title, className = "" }) => {
+	const router = useRouter()
 	return (
 		<Link href={href} className={`${className} relative group`}>
 			{title}
-			<span className="h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300">
+			<span className={`h-[1px] inline-block bg-dark absolute left-0 bottom-0 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`}>
 				&nbsp;
 			</span>
 		</Link>
