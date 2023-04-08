697235cba5a1d723c7c2e778d028bacd08d440ed
arkmadj <ahmadjinadu@gmail.com>
2023-04-08 23:50:52 +0100

    added styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Layout from './Layout'
-import Link from 'next/link'
+import React from "react";
+import Layout from "./Layout";
+import Link from "next/link";
 
 const Footer = () => {
-  return (
-    <footer>
-      <Layout>
-        <span>Rights</span>
-        <Link href="/">Zultan</Link>
-        <Link href="/">Say hello</Link>
-      </Layout>
-    </footer>
-  )
-}
+	return (
+		<footer className="w-full text-lg font-medium border-t-2 border-solid border-dark">
+			<Layout className="flex items-center justify-between py-8">
+				<span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
+				<Link href="/">Zultan</Link>
+				<Link href="/">Say hello</Link>
+			</Layout>
+		</footer>
+	);
+};
 
-export default Footer
+export default Footer;
