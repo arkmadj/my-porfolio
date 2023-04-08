89bb3426f54809eddc89e694ff9065562eef697a
arkmadj <ahmadjinadu@gmail.com>
2023-04-08 23:54:23 +0100

    added link styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
 		<footer className="w-full text-lg font-medium border-t-2 border-solid border-dark">
 			<Layout className="flex items-center justify-between py-8">
 				<span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
-				<div className="flex flex-row">
+				<div className="flex items-center">
 					Build with <span className="px-1 text-2xl text-primary">&#9825;</span>
-					<Link href="/">Zultan</Link>
+					by&nbsp;<Link href="/" className="underline underline-offset-2">Zultan</Link>
 				</div>
 				<Link href="/">Say hello</Link>
 			</Layout>
