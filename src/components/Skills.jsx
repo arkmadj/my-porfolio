21786b82619a30e33117446976514b52be0ff117
arkmadj <ahmadjinadu@gmail.com>
2023-04-12 06:15:28 +0100

    added skill circles

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,10 @@
 const Skills = () => {
-  return (
-    <div>Skills</div>
-  )
-}
+	return (
+		<>
+			<h2 className="w-full mt-64 font-bold text-center text-8xl">Skills</h2>
+			<div className="relative flex items-center justify-center w-full h-screen rounded-full bg-circularLight"></div>
+		</>
+	);
+};
 
-export default Skills
+export default Skills;
