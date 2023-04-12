6f628201db46ea033db3bd0ad881253ebb76e4f4
arkmadj <ahmadjinadu@gmail.com>
2023-04-12 06:20:08 +0100

    added motion to center skill

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,19 @@
+import { motion } from "framer-motion";
+
+
+
 const Skills = () => {
 	return (
 		<>
 			<h2 className="w-full mt-64 font-bold text-center text-8xl">Skills</h2>
-			<div className="relative flex items-center justify-center w-full h-screen rounded-full bg-circularLight"></div>
+			<div className="relative flex items-center justify-center w-full h-screen rounded-full bg-circularLight">
+				<motion.div
+					className="flex items-center justify-center p-8 font-semibold rounded-full bg-dark shadow-dark text-light"
+					whileHover={{ scale: 1.05 }}
+				>
+					Web
+				</motion.div>
+			</div>
 		</>
 	);
 };
