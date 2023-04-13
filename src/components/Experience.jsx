f02444e745f27795a49b9ce680dc258cc3ee7742
arkmadj <ahmadjinadu@gmail.com>
2023-04-13 06:09:23 +0100

    added experience component

---
+++ src/components/Experience.jsx
@@ -0,0 +1,14 @@
+const Experience = () => {
+	return (
+		<div className="my-64">
+			<h2 className="w-full mb-32 font-bold text-center">Experience</h2>
+      <div>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+		</div>
+	);
+};
+
+export default Experience;
