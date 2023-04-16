4d32af96fa92c5e2bf55d5d407e6576201e96dd8
arkmadj <ahmadjinadu@gmail.com>
2023-04-16 20:10:04 +0100

    added mobile navbar

--- src/components/HireMe.jsx
+++ src/components/HireMe.jsx
@@ -4,13 +4,17 @@ import Link from "next/link";
 
 const HireMe = () => {
 	return (
-		<div className="fixed flex items-center justify-center overflow-hidden left-4 bottom-4">
-			<div className="relative flex items-center justify-center w-48 h-auto">
-				<CircularText className={"fill-dark animate-spin-slow dark:fill-light"} />
+		<div className="fixed flex items-center justify-center overflow-hidden left-4 bottom-4 md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute">
+			<div className="relative flex items-center justify-center w-48 h-auto md:w-24">
+				<CircularText
+					className={"fill-dark animate-spin-slow dark:fill-light"}
+				/>
 				<Link
 					href="mailto:abcd@gmail.com"
-					className="absolute flex items-center justify-center w-20 font-semibold -translate-x-1/2 -translate-y-1/2 border border-solid rounded-full shadow-md left-1/2 top-1/2 bg-dark text-light border-dark aspect-square hover:bg-light hover:text-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light"
-				>Hire me</Link>
+					className="absolute flex items-center justify-center w-20 font-semibold -translate-x-1/2 -translate-y-1/2 border border-solid rounded-full shadow-md left-1/2 top-1/2 bg-dark text-light border-dark aspect-square hover:bg-light hover:text-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:w-12 md:h-12 md:text-[10px]"
+				>
+					Hire me
+				</Link>
 			</div>
 		</div>
 	);
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -31,7 +31,7 @@ export default function Home() {
 								text="Turning Vision Into Reality With Code And Design."
 								className="!text-6xl !text-left xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
 							/>
-							<p className="my-4 text-base font-medium">
+							<p className="my-4 text-base font-medium md:text-sm sm:text-xs">
 								As a skilled full-stack developer, I am dedicated to turning
 								ideas into innovative web applications. Explore my latest
 								projects and articles, showcasing my expertise in React.js and
@@ -41,7 +41,7 @@ export default function Home() {
 								<Link
 									href="/dummy.pdf"
 									target={"_blank"}
-									className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light"
+									className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:p-2 md:px-4 md:text-base"
 									download={true}
 								>
 									Resume
@@ -50,7 +50,7 @@ export default function Home() {
 								<Link
 									href="mailto:ahmadjinadu@gmail.com"
 									target={"_blank"}
-									className="text-lg font-medium underline capitalize text-dark dark:text-light"
+									className="text-lg font-medium underline capitalize text-dark dark:text-light md:text-base"
 								>
 									Contact
 								</Link>
