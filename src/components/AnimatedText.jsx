e67ea0988696bc2a0ddabd1b4167f65a7d4f300d
arkmadj <ahmadjinadu@gmail.com>
2023-04-16 19:55:43 +0100

    added mobile navbar

--- src/components/AnimatedText.jsx
+++ src/components/AnimatedText.jsx
@@ -29,7 +29,7 @@ const singleWord = {
 
 const AnimatedText = ({ text, className = "" }) => {
 	return (
-		<div className="flex items-center justify-center w-full py-2 mx-auto overflow-hidden text-center">
+		<div className="flex items-center justify-center w-full py-2 mx-auto overflow-hidden text-center sm:py-0">
 			<motion.h1
 				className={`inline-block w-full text-dark font-bold capitalize text-8xl ${className} dark:text-light`}
 				variants={quote}
